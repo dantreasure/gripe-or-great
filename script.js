bfabaf88a2aa5485cdec2a55091092f8e47e12b1
Daniel Treasure <dbtreasure@gmail.com>
2013-10-11 01:22:21 -0600

    added jquery animations and descriptions for the gripe, and great stats options

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 $(document).ready(function(){
+	var myRootRef = new Firebase('https://plains-of-despair.firebaseio.com/');
+myRootRef.set('Hello World!');
+	// Function to hide all options
 	function hideAll(){
 		$('#gripe').hide('slow');
 		$('#great').hide('slow');
 		$('#stats').hide('slow');
 	}
+
+
 	$('#gripe-btn').click(function(){
-		hideAll();
-		$('#gripe').show('slow');
+		if ($('#gripe').is(":visible")){
+			return;
+		}
+		else{
+			hideAll();
+			$('#gripe').show('slow');	
+		}
 	});	
 	$('#great-btn').click(function(){
-		hideAll();
-		$('#great').show('slow');
+		if ($('#great').is(":visible")){
+			return;
+		}
+		else{
+			hideAll();
+			$('#great').show('slow');
+		}
 	});	
 	$('#stats-btn').click(function(){
-		hideAll();
-		$('#stats').show('slow');
+		if ($('#stats').is(":visible")){
+			return;
+		}
+		else{
+			hideAll();
+			$('#stats').show('slow');
+		}
 	});	
 });
