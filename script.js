8e5409882e6b50def093574df657478f09620c06
Daniel Treasure <dbtreasure@gmail.com>
2013-11-15 19:51:30 -0700

    DRY-ed up the character tracking on submit button, and the reveal function for the Gripe and Great buttons

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
-	/*
-	 * Track presence of charactesr in text area to enable submit button
-	 */
-	 function trackGripeChar(val){
-	 	if (val.value.length !== 0){
-	 		$('#gripe-submit').removeClass('disabled');
-	 	}
-	 	if(val.value.length == 0){
-	 		$('#gripe-submit').addClass('disabled');
-	 	}
-	 }
+/*
+ * Track presence of charactesr in text area to enable submit button
+ */
 
-	 function trackGreatChar(val){
-	 	if (val.value.length !== 0){
-	 		$('#great-submit').removeClass('disabled');
-	 	}
-	 	if(val.value.length == 0){
-	 		$('#great-submit').addClass('disabled');
-	 	}
-	 }
+function trackChar(val, selector){
+ 	if (val.value.length !== 0){
+ 		$('#' + selector + '-submit').removeClass('disabled');
+ 	}
+ 	else{
+ 		$('#' + selector + '-submit').addClass('disabled');
+ 	}
+ }
 
 $(document).ready(function(){
 	/*
@@ -29,73 +21,66 @@ $(document).ready(function(){
 		$('#stats').hide('slow');
 	}
 
-
-
 	/*
 	 * Hide/ Show jQuery on the options
 	 */
-	$('#gripe-btn').click(function(){
-		if ($('#gripe').is(":visible")){
-			return;
-		}
-		else{
-			hideAll();
-			$('#gripe').show('slow');
-		}
-	});	
-	$('#great-btn').click(function(){
-		if ($('#great').is(":visible")){
-			return;
-		}
-		else{
-			hideAll();
-			$('#great').show('slow');
-		}
-	});	
+	function reveal(selector){
+		$('#' + selector + '-btn').click(function(){
+			if ($('#' + selector).is(":visible")){
+				return;
+			}
+			else{
+				hideAll();
+				$('#' + selector).show('slow');
+			}
+		});
+	}
+	reveal('gripe');
+	reveal('great');
 });
 
-	/*
-	 * Firebase Logic
-	 */
+/*
+ * Firebase Logic
+ */
 
-	var gripeRef = new Firebase('https://gripe-or-great.firebaseio.com/gripe');
-	var greatRef = new Firebase('https://gripe-or-great.firebaseio.com/great');
-	
-
-	$('#gripe-submit').click(function(){
-		var gripeText = $('#gripe-textarea').val();
-		var submissionType = 'gripe';
-		gripeRef.push({submissionType: submissionType, gripeText: gripeText});
-		$('#gripe-textarea').val('');
-	});
-	$('#great-submit').click(function(){
-	 	var greatText = $('#great-textarea').val();
-		greatRef.push({"title": "great", "text": greatText});
-		$('#great-textarea').val('');
-	});
+var gripeRef = new Firebase('https://gripe-or-great.firebaseio.com/gripe');
+var greatRef = new Firebase('https://gripe-or-great.firebaseio.com/great');
 
 
-	gripeRef.on('value', function(snapshot){
-		var gripeCount = 0;
-		snapshot.forEach(function() {
-       		gripeCount++;
-  		});
-  		$('#numberOfGripes').html('Number of Gripes: ' + gripeCount);
-	});
+$('#gripe-submit').click(function(){
+	var gripeText = $('#gripe-textarea').val();
+	var submissionType = 'gripe';
+	gripeRef.push({submissionType: submissionType, gripeText: gripeText});
+	$('#gripe-textarea').val('');
+});
 
-	gripeRef.on('child_added', function(snapshot){
-		$('#listOfGripes').append('<h3>' + snapshot.val().gripeText + '</h3>')
-	});
+$('#great-submit').click(function(){
+ 	var greatText = $('#great-textarea').val();
+	greatRef.push({"title": "great", "text": greatText});
+	$('#great-textarea').val('');
+});
 
-	greatRef.on('value', function(snapshot){
-		var greatCount = 0;
-		snapshot.forEach(function() {
-       		greatCount++;
-  		});
-  		$('#numberOfGreats').html('Number of Greats: ' + greatCount);
+gripeRef.on('value', function(snapshot){
+	var gripeCount = 0;
+	snapshot.forEach(function() {
+   		gripeCount++;
+		});
+		$('#numberOfGripes').html('Number of Gripes: ' + gripeCount);
+});
 
-	});
+gripeRef.on('child_added', function(snapshot){
+	$('#listOfGripes').append('<h3>' + snapshot.val().gripeText + '</h3>')
+});
 
-	greatRef.on('child_added', function(snapshot){
-		$('#listOfGreats').append('<h3>' + snapshot.val().text + '</h3>')
-	});
+greatRef.on('value', function(snapshot){
+	var greatCount = 0;
+	snapshot.forEach(function() {
+   		greatCount++;
+		});
+		$('#numberOfGreats').html('Number of Greats: ' + greatCount);
+
+});
+
+greatRef.on('child_added', function(snapshot){
+	$('#listOfGreats').append('<h3>' + snapshot.val().text + '</h3>')
+});
